3a281d39582aea5010082b4f91af24051d940dca
Degover <chaors2000@gmail.com>
2022-08-13 13:28:17 -0300

    ✨ Created the patient controller

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import express  from 'express'
 import ip from 'ip'
 import dotenv from 'dotenv'
 import cors from 'cors'
+import HttpStatus from './controller/patient.controller.js'
 import Response from './domain/response.js'
 import log from './util/logger.js'
 
@@ -12,5 +13,5 @@ const app = express()
 app.use(cors({ origin: '*' }))
 app.use(express.json())
 
-app.get('/', (req, res) => res.send(new Response(200, 'OK', 'Patient API, v1.0.0 - All Systems Go')))
+app.get('/', (req, res) => res.send(new Response(HttpStatus.OK.code, HttpStatus.OK.status, 'Patient API, v1.0.0 - All Systems Go')))
 app.listen(PORT, () => log.info(`Server running on ${ip.address()}:${PORT}`))
