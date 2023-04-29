8020b8850ac529ad394ae487fb8ff1263b7db212
Degover <chaors2000@gmail.com>
2023-04-29 19:36:21 -0300

    ✨ Criado card de pensamentos

--- angular/memoteca/src/app/componentes/pensamentos/listar-pensamento/listar-pensamento.component.ts
+++ angular/memoteca/src/app/componentes/pensamentos/listar-pensamento/listar-pensamento.component.ts
@@ -6,6 +6,26 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./listar-pensamento.component.css']
 })
 export class ListarPensamentoComponent implements OnInit {
+  listaPensamentos = [
+    {
+      id: '1',
+      conteudo: 'Aprendendo Angular a',
+      autoria: 'Dev',
+      modelo: 'modelo3'
+    },
+    {
+      id: '1',
+      conteudo: 'Aprendendo Angular a',
+      autoria: 'Dev',
+      modelo: 'modelo3'
+    },
+    {
+      id: '1',
+      conteudo: 'Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Donec quam felis, ultricies nec, pellentesque eu, pretium quis, sem. Nulla consequat massa quis enim. Donec.',
+      autoria: 'Outrem',
+      modelo: 'modelo2'
+    }
+  ];
 
   constructor() { }
 
---
+++ angular/memoteca/src/app/componentes/pensamentos/pensamento/pensamento.component.ts
@@ -0,0 +1,26 @@
+import { Component, Input, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-pensamento',
+  templateUrl: './pensamento.component.html',
+  styleUrls: ['./pensamento.component.css']
+})
+export class PensamentoComponent implements OnInit {
+  @Input() pensamento = {
+    id: '1',
+    conteudo: 'Aprendendo Angular',
+    autoria: 'Dev',
+    modelo: 'modelo3'
+  }
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+  larguraPensamento(): string {
+    if (this.pensamento.conteudo.length >= 256)
+      return "pensamento-g"
+    else return "pensamento-p"
+  }
+}
