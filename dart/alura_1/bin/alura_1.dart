61818f414bad15b5f44647aa5ead7c1e753222de
Degover <chaors2000@gmail.com>
2023-07-16 14:01:12 -0300

    🎉 Created project to study Dart

---
+++ dart/alura_1/bin/alura_1.dart
@@ -0,0 +1,5 @@
+import 'package:alura_1/alura_1.dart' as alura_1;
+
+void main(List<String> arguments) {
+  print('Hello world: ${alura_1.calculate()}!');
+}
---
+++ dart/alura_1/test/alura_1_test.dart
@@ -0,0 +1,8 @@
+import 'package:alura_1/alura_1.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
