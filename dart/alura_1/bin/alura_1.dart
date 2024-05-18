932dc53d9fe2c10de6f2d7f8425d681952e99dd8
Degover <chaors2000@gmail.com>
2024-05-18 13:26:58 -0300

    Learning Dart

--- dart/alura_1/bin/alura_1.dart
+++ dart/alura_1/bin/alura_1.dart
@@ -1,5 +1,22 @@
 import 'package:alura_1/alura_1.dart' as alura_1;
 
 void main(List<String> arguments) {
+  const idade = 256;
+  final altura = 1.25;
+  var test = idade != altura;
+  var other_test = ["test"];
+  other_test.every((test) => test != '');
+
   print('Hello world: ${alura_1.calculate()}!');
+
+  switch (idade) {
+    case 1:
+    case 2:
+    case 256:
+      print('object');
+      break;
+    default:
+      print('default');
+      break;
+  }
 }
